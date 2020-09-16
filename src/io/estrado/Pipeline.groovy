8b645ca270a8e5ac733575b4aef4d4e4c9157e79
Victor I <vifarinde@gmail.com>
2020-09-15 22:39:54 -0500

    Updating

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -48,12 +48,12 @@ def helmDeploy(Map args) {
     if (args.dry_run) {
         println "Running dry-run deployment"
 
-        sh "helm install --namespace ${namespace} -f helm/${args.values_file}-${args.namespace}.yaml --dry-run --debug ${args.name} ${args.chart_dir}"
+        sh "helm install --namespace ${args.namespace} -f helm/${args.values_file}-${namespace}.yaml --dry-run --debug ${args.name}-${args.namespace} ${args.name}"
     } else {
         println "Running deployment"
 
         //sh "helm dependency update ${args.chart_dir}"
-        sh "helm upgrade --namespace ${namespace} -f helm/${args.values_file}-${env.namespace}.yaml  --install ${args.name}-${env.namespace} ${args.name}"
+        sh "helm upgrade --namespace ${args.namespace} -f helm/${args.values_file}-${namespace}.yaml --install ${args.name}-${args.namespace} ${args.name}"
 
         echo "Application ${args.name} successfully deployed. Use helm status ${args.name} to check"
 
