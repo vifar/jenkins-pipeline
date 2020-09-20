2741fc1ce150cfb1e72e617ebae02eb07be1ed8c
Victor I <vifarinde@gmail.com>
2020-09-20 11:06:01 -0500

    Test

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -37,7 +37,7 @@ def helmDeploy(Map args) {
     if (args.dry_run) {
         println "Running dry-run deployment"
 
-        sh "helm upgrade ${args.name} ${args.name}-${namespace} --install --force --namespace ${namespace} -f ${args.name}/values-${namespace}.yaml --dry-run --debug "
+        sh "helm upgrade ${args.name} ${args.name}-${namespace} --set rbac.clusterAdminRole=true --install --force --namespace ${namespace} -f ${args.name}/values-${namespace}.yaml --dry-run --debug"
     } else {
         println "Running deployment"
 
