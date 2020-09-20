0c3df55569641b67291b6eb3ae40088fd979dfc3
Victor I <vifarinde@gmail.com>
2020-09-20 10:32:41 -0500

    Froce

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -37,7 +37,7 @@ def helmDeploy(Map args) {
     if (args.dry_run) {
         println "Running dry-run deployment"
 
-        sh "helm upgrade --install --set ingressController.installCRDs=false --namespace ${namespace} -f ${args.name}/values-${namespace}.yaml --dry-run --debug ${args.name} --generate-name"
+        sh "helm upgrade --install --force --namespace ${namespace} -f ${args.name}/values-${namespace}.yaml --dry-run --debug ${args.name} --generate-name"
     } else {
         println "Running deployment"
 
